b7a7b57a35a3e787e96fb6ef20cc74c2ee5e4ec8
Ferenc Sipos <ferenc.m.sipos@gmail.com>
2022-06-24 21:25:06 +0200

    eventcollapser, buttons, card/event models

---
+++ PracticeBlazorServerProject/Models/Card.cs
@@ -0,0 +1,13 @@
+﻿namespace PracticeBlazorServerProject.Models
+{
+    public class Card
+    {
+        public string BackgroundUrl { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+
+    }
+}
---
+++ PracticeBlazorServerProject/Models/Event.cs
@@ -0,0 +1,9 @@
+﻿namespace PracticeBlazorServerProject.Models
+{
+    public class Event
+    {
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public List<Card> Cards { get; set; }
+    }
+}
